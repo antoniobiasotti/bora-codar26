8a5eccebd61696c4eb47ff27745f448bc9d400bc
antoniobiasotti <abamorim36@gmail.com>
2024-05-01 16:36:24 -0300

    working on Pamonha App

---
+++ src/components/Pamonha.jsx
@@ -0,0 +1,10 @@
+import styles from './Pamonha.css'
+
+export function Pamonha(){
+    return (
+        <div className={styles.Pamonha}>
+            <div>Pamonha</div>
+            <div>Prato típico com milho verde ralado</div>
+        </div>
+    )
+}
